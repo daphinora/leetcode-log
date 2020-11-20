374122ca09db3a86e8d2cd355b8158a856239394
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-11-20 09:58:23 -0600

    125 Valid Palindrome

---
+++ 125 Valid Palindrome.js
@@ -0,0 +1,12 @@
+// Problem:
+// Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+// Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+// Solution:
+const s = "A man, a plan, a canal: Panama"
+
+var isPalindrome = function (s) {
+    const string = s.replace(/[^a-z0-9]/gi, '').toLowerCase()
+    return string === string.split('').reverse().join('')
+}
