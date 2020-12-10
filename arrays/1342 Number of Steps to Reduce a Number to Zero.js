4889b8e4db18d633097068c10b3be567672b0c9e
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-12-09 21:46:46 -0600

    1342 Number of Steps to Reduce a Number to Zero

---
+++ arrays/1342 Number of Steps to Reduce a Number to Zero.js
@@ -0,0 +1,19 @@
+// Problem:
+// Given a non-negative integer num, return the number of steps to reduce it to zero.
+// If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+// Solution:
+var numberOfSteps  = function(num) {
+    let steps = 0
+    while (num > 0) {
+        // use a ternary to check if the current num is even
+        num % 2 == 0 
+        // if the num is even, increase our steps counter and divide num by two
+        ? (++steps) && (num /= 2) 
+        // if the num is odd, increase our steps counter and subtract one from num
+        : (++steps) && (num -= 1)
+    } return steps
+};
+
+const num = 14;
+numberOfSteps(num)
--- arrays/412 Fizz Buzz.js
+++ arrays/412 Fizz Buzz.js
@@ -9,14 +9,28 @@ var fizzBuzz = function(n) {
     for (let i = 1; i <= n; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
             arr.push("FizzBuzz")
+            // if the number we're looking at can be
+            // divided evenly by both 3 and 5
+            // add FizzBuzz to our array
         } else if (i % 3 === 0) {
             arr.push("Fizz")
+            // if our number can only be divided
+            // evenly by 3 but not 5 then
+            // add Fizz to our array
         } else if (i % 5 === 0) {
             arr.push("Buzz")
+            // if our number can only be divided
+            // evenly by 5 but not 3 then
+            // add Buzz to our array
         } else {arr.push(`${i}`)}
+            // if our number cannot be divided
+            // cleanly by 3 or 5 then just
+            // add the number itself to our array
     }
     return arr
 };
 
 const n = 15;
+// fizzBuzz will create an array full of numbers from 1 to n and
+// replace every multiple of 3, 5, or 3 and 5 with different strings
 fizzBuzz(n)
