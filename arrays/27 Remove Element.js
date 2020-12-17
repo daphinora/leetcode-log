c89b7a83ab363303dbd7c7a309ee75070e811cca
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-12-17 14:40:08 -0600

    27 Remove Element

---
+++ arrays/27 Remove Element.js
@@ -0,0 +1,34 @@
+// Problem:
+// Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+// Do not allocate extra space for another array, you must do this by modifying the input 
+// array in-place with O(1) extra memory.
+
+// The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+// Clarification:
+
+// Confused why the returned value is an integer but your answer is an array?
+
+// Note that the input array is passed in by reference, which means a modification to the input 
+// array will be known to the caller as well.
+
+// Solution:
+
+var removeElement = function(nums, val) {
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] === val) {
+            nums.splice(i, 1);
+            i--
+        }
+    }
+    return nums
+    // in the original leetcode problem, they were already tracking nums. returning nums was redundant and
+    // would only return an empty array. however, for the sake of this entry (and testing), i decided to return
+    // nums here in my log. :)
+};
+
+const nums = [3,2,2,3]
+const val = 3
+
+console.log(removeElement(nums,val));
