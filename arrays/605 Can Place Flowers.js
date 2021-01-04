8ee365a844b3cac3250fe4d279afe1d3db8ff438
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2021-01-04 15:21:58 -0600

    added notation on 605 Can Place FLowers

--- arrays/605 Can Place Flowers.js
+++ arrays/605 Can Place Flowers.js
@@ -8,18 +8,31 @@
 //  without violating the no-adjacent-flowers rule.
 
 // Solution:
-var canPlaceFlowers = function(flowerbed, n) {
-    for(let i = 0; i <= flowerbed.length; i++) {
-        if(n > 0) {
-            if(flowerbed[i] == 0 && (flowerbed[i+1] == 0 || i == flowerbed.length - 1) && (flowerbed[i-1] == 0 || i == 0)) {
+var canPlaceFlowers = function (flowerbed, n) {
+    for (let i = 0; i <= flowerbed.length; i++) {
+        if (n > 0) {
+            // so if we still have flowers to add into our flowerbed, we check the following
+            if (flowerbed[i] == 0 &&
+                // if the current element equals 0
+                (flowerbed[i + 1] == 0 || i == flowerbed.length - 1) &&
+                // and the next element equals zero OR the current element is the last element in the array
+                (flowerbed[i - 1] == 0 || i == 0)
+                // and the previous element equals zero OR the current element is the first in the array
+                ) {
                 flowerbed.splice(i, 1, 1);
+                // then replace the current 0 with a 1
                 n -= 1;
+                // and decrease the number of flowers we need to put into the flowerbed
             }
-        } else {return true}
+        } else {
+            // if we planted all of our flowers into the flowerbed, return true! well done!
+            return true 
+        }
     }
+    // this will only hit if we weren't able to put all of our flowers into the flowerbed
     return false
 };
 
-const flowerbed = [1,0,0,0,1];
+const flowerbed = [1, 0, 0, 0, 1];
 const n = 1;
 console.log(canPlaceFlowers(flowerbed, n));
